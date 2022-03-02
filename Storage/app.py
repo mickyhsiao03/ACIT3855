5a88af9cd27465d34de96cda38f41ee6d72db8aa
Shih Chieh Hsiao <mickyhsiao03@hotmail.com>
2022-03-01 23:58:53 -0800

    added sample code

--- Storage/app.py
+++ Storage/app.py
@@ -19,7 +19,7 @@ from pykafka import KafkaClient
 from pykafka.common import OffsetType
 from threading import Thread
 
-with open('app_conf.yml', 'r') as f: 
+with open('./app_conf.yml', 'r') as f: 
     app_config = yaml.safe_load(f.read())
 with open('log_conf.yml', 'r') as f: 
     log_config = yaml.safe_load(f.read()) 
