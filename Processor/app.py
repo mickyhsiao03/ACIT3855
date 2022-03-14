d0645d15fed220313493aaaa5425ce61673c36eb
Shih Chieh Hsiao <mickyhsiao03@hotmail.com>
2022-03-13 19:19:00 -0700

    testing

--- Processor/app.py
+++ Processor/app.py
@@ -42,15 +42,15 @@ def populate():
     for reading in readings: 
         read_list.append(reading.to_dict()) 
     
-    
+    current_timestamp = datetime.strptime(str(datetime.now()),"%Y-%m-%d %H:%M:%S.%f").strftime("%Y-%m-%d %H:%M:%S.%f") 
 
     if len(read_list) == 0:
-        timestamp = datetime.strptime(str(datetime.now()),"%Y-%m-%d %H:%M:%S.%f").strftime("%Y-%m-%dT%H:%M:%S") 
+        timestamp = current_timestamp
 
     else:
         timestamp = datetime.strptime(read_list[len(read_list)-1]['last_updated'], "%Y-%m-%dT%H:%M:%S").strftime("%Y-%m-%d %H:%M:%S.%f")
 
-    current_timestamp = datetime.strptime(str(datetime.now()),"%Y-%m-%d %H:%M:%S.%f").strftime("%Y-%m-%d %H:%M:%S.%f") 
+
     #get data from storage service
     headers = {"content-type": "application/json"}
     stock_response = requests.get(app_config['eventstore']['url']+timestamp+"&end_timestamp="+current_timestamp, headers=headers)
