4d17b1115305b70f8ceeeb3e588e36fe1b0e69c3
Shih Chieh Hsiao <mickyhsiao03@hotmail.com>
2022-03-24 01:28:55 -0700

    external config changes

--- Processor/app.py
+++ Processor/app.py
@@ -19,13 +19,27 @@ from stats import Stats as Stats
 from apscheduler.schedulers.background import BackgroundScheduler
 from flask_cors import CORS, cross_origin
 
-with open('./app_conf.yml', 'r') as f: 
+if "TARGET_ENV" in os.environ and os.environ["TARGET_ENV"] == "test":
+    print("In Test Environment")
+    app_conf_file = "/config/app_conf.yml"
+    log_conf_file = "/config/log_conf.yml"
+else:
+    print("In Dev Environment")
+    app_conf_file = "app_conf.yml"
+    log_conf_file = "log_conf.yml"
+
+with open(app_conf_file, 'r') as f:
     app_config = yaml.safe_load(f.read())
-with open('log_conf.yml', 'r') as f: 
+
+# External Logging Configuration
+with open(log_conf_file, 'r') as f:
     log_config = yaml.safe_load(f.read())
     logging.config.dictConfig(log_config)
 
 logger = logging.getLogger('basicLogger')
+
+logger.info("App Conf File: %s" % app_conf_file)
+logger.info("Log Conf File: %s" % log_conf_file)
 
 DB_ENGINE = create_engine("sqlite:///%s" % (app_config["datastore"]["filename"]))
 Base.metadata.bind = DB_ENGINE 
